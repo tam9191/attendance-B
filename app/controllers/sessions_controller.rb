babd913f4015d177d34cfd5e14c7322bc776b990
mt9191 <m.t.habite@gmail.com>
2019-09-27 08:56:43 +0000

    6.3.1

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:ssesion][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "認証に失敗しました"
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   root 'static_pages#top'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   
