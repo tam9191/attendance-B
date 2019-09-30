c50b144c89c20a06f06bad5772df2c655cfaa8d2
mt9191 <m.t.habite@gmail.com>
2019-09-30 05:51:11 +0000

    Finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
   
   def show
   end
@@ -33,6 +39,12 @@ class UsersController < ApplicationController
     end
   end 
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました"
+    redirect_to users_url
+  end
+  
   private
     
     def set_user
@@ -52,8 +64,10 @@ class UsersController < ApplicationController
     end
     
     def correct_user
-      @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
     
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
